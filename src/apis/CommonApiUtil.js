64d649b83fc044b010883fe39264557e75f7ef91
Paras Bhatt <parryapplications@gmail.com>
2025-04-01 15:50:06 +0530

    Public our env variables for Vercel

--- src/apis/CommonApiUtil.js
+++ src/apis/CommonApiUtil.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 //Dynamic URLs fetch:
-export const BACKEND_BASE_URL = process.env.BACKEND_BASE_URL;
-export const FRONTEND_BASE_URL = process.env.FRONTEND_BASE_URL;
+export const BACKEND_BASE_URL = process.env.NEXT_PUBLIC_BACKEND_BASE_URL;
+export const FRONTEND_BASE_URL = process.env.NEXT_PUBLIC_FRONTEND_BASE_URL;
 
 export const eventsHubApiClient = axios.create({
   baseURL: BACKEND_BASE_URL,
