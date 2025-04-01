c6959b31e0d7005490335a09bc6c3cf770a119e2
Paras Bhatt <parryapplications@gmail.com>
2025-04-01 16:58:06 +0530

    Fixed env fetch
    As per Vite framework, we should use this import.meta.env...

--- src/apis/CommonApiUtil.js
+++ src/apis/CommonApiUtil.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 //Dynamic URLs fetch:
-export const BACKEND_BASE_URL = process.env.NEXT_PUBLIC_BACKEND_BASE_URL;
-export const FRONTEND_BASE_URL = process.env.NEXT_PUBLIC_FRONTEND_BASE_URL;
+export const BACKEND_BASE_URL = import.meta.env.VITE_BACKEND_BASE_URL;
+export const FRONTEND_BASE_URL = import.meta.env.VITE_FRONTEND_BASE_URL;
 
 export const eventsHubApiClient = axios.create({
   baseURL: BACKEND_BASE_URL,
